2802d78543cf260dd19b9cd2e11a2eb6336fc4a1
William Due <williamhenrichdue@gmail.com>
2021-12-01 13:24:20 +0100

    Day 2, Part 2

---
+++ Day1/part2.hs
@@ -0,0 +1,11 @@
+import Data.Set ( fromList, toList )
+
+toIntList :: String -> [Integer]
+toIntList = fmap (\n -> read n :: Integer) . lines
+
+main :: IO ()
+main = do
+      content <- getContents
+      let list' = toIntList content
+      let listSum = zipWith3 (\a b c -> a + b + c) list' (tail list') (tail $ tail list')
+      print . length . filter (True==) . zipWith (<) listSum $ tail listSum
