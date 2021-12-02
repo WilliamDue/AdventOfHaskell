cc551470219ea448a40a9e6016c2182c9dbd8c78
William Due <williamhenrichdue@gmail.com>
2021-12-02 12:49:09 +0100

    part 1 is done

---
+++ Day2/part2.hs
@@ -0,0 +1,24 @@
+import Data.Maybe ( fromMaybe )
+
+
+clean :: String -> [(String, Integer)]
+clean = fromMaybe [] . mapM (toTuple . words) . lines
+            where toInt n = read n :: Integer
+                  toTuple [a, b] = Just (a, toInt b)
+                  toTuple x = Nothing
+
+program :: [(String, Integer)] -> (Integer, Integer) -> Maybe (Integer, Integer)
+program [] (a, b) = Just (a, b)
+program (x:xs) (a, b)
+      | movement == "forward" = program xs (a + scaler, b)
+      | movement == "down" = program xs (a, b + scaler)
+      | movement == "up" = program xs  (a, b - scaler)
+      | otherwise = Nothing
+      where movement = fst x
+            scaler = snd x
+
+main :: IO ()
+main = do
+      content <- getContents
+      let list' = clean content
+      print . fmap (uncurry (*)) $ program list' (0, 0)
