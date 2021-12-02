d39ff29a95b2475f501e72657c75c697faa006a7
William Due <williamhenrichdue@gmail.com>
2021-12-02 17:15:19 +0100

    clean up

--- Day2/part2_1.hs
+++ Day2/part2_1.hs
@@ -1,19 +1,19 @@
 import Data.Maybe ( fromMaybe )
 
 
-clean :: String -> [(String, Integer)]
-clean = fromMaybe [] . mapM (toTuple . words) . lines
-            where toTuple [a, b] = Just (a, read b :: Integer)
-                  toTuple _ = Nothing
-
 func :: (String, Integer) -> ((Integer, Integer, Integer) -> (Integer, Integer, Integer))
 func ("down", x) = \(horiz, depth, aim) -> (horiz, depth, aim + x)
 func ("up", x) = \(horiz, depth, aim) -> (horiz, depth, aim - x)
 func ("forward", x) = \(horiz, depth, aim) -> (horiz + x, depth + x * aim, aim)
 func _ = id
 
+parse :: String -> [(Integer, Integer, Integer) -> (Integer, Integer, Integer)]
+parse = fromMaybe [] . mapM (fmap func . format . words) . lines
+            where format [a, b] = Just (a, read b :: Integer)
+                  format _ = Nothing
+
 program :: String -> (Integer, Integer, Integer) -> (Integer, Integer, Integer)
-program = foldl (flip (.)) id . map func . clean
+program = foldl1 (flip (.)) . parse
 
 main :: IO ()
 main = do
