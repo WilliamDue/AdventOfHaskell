68cf7d177151049acdc440f38bc9817fac5e21fe
William Due <williamhenrichdue@gmail.com>
2021-12-06 09:49:08 +0100

    Even more clean up

--- Day6/part1and2.hs
+++ Day6/part1and2.hs
@@ -1,5 +1,4 @@
 import Data.List.Split ( splitOn )
-import Data.Maybe ( fromMaybe )
 import qualified Data.Map as Map
 
 
@@ -11,14 +10,13 @@ func n = Map.unionWith (+) n' mOne'
       where (mOne, n') = Map.partitionWithKey (\k _ -> k == -1) $ Map.mapKeys (\k -> k - 1) n
             mOne' = (\m -> Map.fromList [(8, m), (6, m)]) $ Map.findWithDefault 0 (-1) mOne
 
-toMap :: [Int] -> Map.Map Int Int
-toMap = foldl1 (Map.unionWith (+)) . map (\n -> Map.fromList [(n, 1)])
-
-count :: Map.Map Int Int -> Int
-count = sum . map snd . Map.toList
+program :: Int -> [Int] -> Int
+program n = count . last . take (n + 1) . iterate func . toMap
+      where toMap = foldl1 (Map.unionWith (+)) . map (\n -> Map.fromList [(n, 1)])
+            count = sum . map snd . Map.toList
 
 main :: IO ()
 main = do
       content <- getContents
-      print . count . last . take 81 . iterate func . toMap $ parse content
-      print . count . last . take 257 . iterate func . toMap $ parse content
+      print . program 80 $ parse content
+      print . program 256 $ parse content
