fa3606688cf3fbde9858d1e32130593413021a7d
William Due <williamhenrichdue@gmail.com>
2021-12-06 09:28:11 +0100

    done

--- Day6/part1and2.hs
+++ Day6/part1and2.hs
@@ -1,5 +1,5 @@
 import Data.List.Split ( splitOn )
-import Data.Maybe ( mapMaybe )
+import Data.Maybe ( fromMaybe )
 import qualified Data.Map as Map
 
 
@@ -7,20 +7,22 @@ parse :: String -> [Int]
 parse = map toInt . splitOn ","
       where toInt n = read n :: Int
 
-func :: Int -> [Int]
-func n = if n - 1 == -1 then [6, 8] else [n - 1]
-
-func' :: Map.Map Int Int -> Map.Map Int Int
-func' n = Map.unionWith (+) n' mOne'
-      where (mOne, n') = Map.partitionWithKey (\k _ -> k == -1) $ Map.mapKeys (\k -> k - 1) n
-            mOne' = Map.fromList [(6, Map.findWithDefault 0 -1 mOne), (8, Map.findWithDefault 0 -1 mOne)]
+func :: Map.Map Int Int -> Map.Map Int Int
+func n = Map.unionWith (+) n'' mOne'
+      where n' = Map.mapKeys (\k -> k - 1) n
+            (mOne, n'') = Map.partitionWithKey (\k _ -> k == -1) n'
+            mOnes = Map.findWithDefault 0 (-1) mOne
+            mOne' = Map.fromList [(8, mOnes), (6, mOnes)]
 
 
 toMap :: [Int] -> Map.Map Int Int
 toMap = foldl1 (Map.unionWith (+)) . map (\n -> Map.fromList [(n, 1)])
 
+count :: Map.Map Int Int -> Int
+count = sum . map snd . Map.toList
+
 main :: IO ()
 main = do
       content <- getContents
-      -- print . length . last . take 81 . iterate (concatMap func) $ parse content
-      print . func' . func' . toMap $ parse content
+      print . count . last . take 81 . iterate func . toMap $ parse content
+      print . count . last . take 257 . iterate func . toMap $ parse content
