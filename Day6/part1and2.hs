8c8baf1ef9b3be842bec52b79d5414358aac3a97
William Due <williamhenrichdue@gmail.com>
2021-12-06 09:56:13 +0100

    better name

--- Day6/part1and2.hs
+++ Day6/part1and2.hs
@@ -10,13 +10,13 @@ func n = Map.unionWith (+) n' mOne'
       where (mOne, n') = Map.partitionWithKey (\k _ -> k == -1) $ Map.mapKeys (\k -> k - 1) n
             mOne' = (\m -> Map.fromList [(8, m), (6, m)]) $ Map.findWithDefault 0 (-1) mOne
 
-program :: Int -> [Int] -> Int
-program n = count . last . take (n + 1) . iterate func . toMap
+countFishAtDay :: Int -> [Int] -> Int
+countFishAtDay n = count . last . take (n + 1) . iterate func . toMap
       where toMap = foldl1 (Map.unionWith (+)) . map (\n -> Map.fromList [(n, 1)])
             count = sum . map snd . Map.toList
 
 main :: IO ()
 main = do
       content <- getContents
-      print . program 80 $ parse content
-      print . program 256 $ parse content
+      print . countFishAtDay 80 $ parse content
+      print . countFishAtDay 256 $ parse content
