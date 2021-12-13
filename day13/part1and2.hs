2a4094b86bf000de6d7557cdffe4ff29720a741f
William Due <williamhenrichdue@gmail.com>
2021-12-13 15:33:53 +0100

    pretty printing functionality

--- day13/part1and2.hs
+++ day13/part1and2.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE TupleSections #-}
-import qualified Data.List as L
 import qualified Data.Set as S
-import Data.List.Split ( splitOn )
+import Data.List.Split ( splitOn, chunksOf )
 
 toOper :: [String] -> (Char, Int)
 toOper [a, b] = (head a, read b)
@@ -14,32 +13,38 @@ toTuple n = error $ "input error " ++ show n
 splitDoubleLine :: String -> (String, String)
 splitDoubleLine = toTuple . splitOn "\n\n"
 
-parse :: String -> ([(Char, Int)], [(Int, Int)])
+parse :: String -> ([(Char, Int)], S.Set (Int, Int))
 parse blob = (oper, paper)
       where (dots, folds) = splitDoubleLine blob
             oper = map ((toOper . splitOn "=") . (last . splitOn "along ")) $ lines folds
-            paper = map (toTuple . map read . splitOn ",") $ lines dots
+            paper = S.fromList . map (toTuple . map read . splitOn ",") $ lines dots
 
 foldPaper :: (Char, Int) -> (Int, Int) -> (Int, Int)
 foldPaper ('y', n) (x, y) = (x, abs $ y - 2*n)
 foldPaper ('x', n) (x, y) = (abs $ x - 2*n, y)
 foldPaper (_, _) (x, y) = (x, y)
 
-partitionBy :: (Char, Int) -> [(Int, Int)] -> ([(Int, Int)], [(Int, Int)])
-partitionBy ('y', n) paper = L.partition ((n<=) . snd) paper
-partitionBy ('x', n) paper = L.partition ((n<=) . fst) paper
+partitionBy :: (Char, Int) -> S.Set (Int, Int) -> (S.Set (Int, Int), S.Set (Int, Int))
+partitionBy ('y', n) paper = S.partition ((n<=) . snd) paper
+partitionBy ('x', n) paper = S.partition ((n<=) . fst) paper
 partitionBy (_, _) paper = error "invalid input"
 
-union :: [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
-union a b = S.toList . S.fromList $ L.union a b
-
-applyFold :: (Char, Int) -> [(Int, Int)] -> [(Int, Int)]
-applyFold oper paper = L.sort $ high' `union` low
+applyFold :: (Char, Int) -> S.Set (Int, Int) -> S.Set (Int, Int)
+applyFold oper paper = high' `S.union` low
       where (high, low) = partitionBy oper paper
-            high' = map (foldPaper oper) high
+            high' = S.map (foldPaper oper) high
 
-applyNFold :: [(Char, Int)] -> [(Int, Int)] -> [(Int, Int)]
+applyNFold :: [(Char, Int)] -> S.Set (Int, Int) -> S.Set (Int, Int)
 applyNFold opers paper  = foldl (flip applyFold) paper opers
+
+prettyPrint :: S.Set (Int, Int) -> IO ()
+prettyPrint n = putStrLn $ unlines canvas''
+      where n' = S.toList n
+            maxX = S.findMax $ S.map fst n
+            maxY = S.findMax $ S.map snd n
+            canvas = [(x, y) | y <- [0..maxY], x <- [0..maxX]]
+            canvas' = map (\k -> if k `elem` n' then '#' else ' ') canvas
+            canvas'' = chunksOf (maxX + 1) canvas'
 
 main :: IO ()
 main = do
@@ -47,8 +52,5 @@ main = do
       let (opers, paper) = parse input
       let part1 = applyFold (head opers) paper
       print $ length part1
-      let temp = applyNFold opers paper
-      let maxX = maximum $ map fst temp
-      let maxY = maximum $ map snd temp
-      let result = temp
-      print result
+      let part2 = applyNFold opers paper
+      prettyPrint part2
