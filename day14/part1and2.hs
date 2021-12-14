e0f03d02347b11307c306f05cc3236f5353cc48f
William Due <williamhenrichdue@gmail.com>
2021-12-14 09:41:00 +0100

    Day 14 done

---
+++ day14/part1and2.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE TupleSections #-}
+import qualified Data.Map as M
+import Data.List.Split ( splitOn )
+import qualified Data.Bifunctor
+
+toTuple :: [b] -> (b, b)
+toTuple [a, b] = (a, b)
+toTuple n = error "input error"
+
+type Operation = ((Char, Char), Char)
+type PolyChains = M.Map (Char, Char) Integer
+type PolyCount = M.Map Char Integer
+type Poly = (PolyChains, PolyCount)
+
+parse :: String -> ([Operation], Poly)
+parse blob = (oper, (poly, polyCount))
+      where (poly', oper') = toTuple $ splitOn "\n\n" blob
+            oper = map (Data.Bifunctor.bimap toTuple head
+                         . toTuple
+                         . splitOn " -> ")
+                    $ lines oper'
+            poly = foldl1 (M.unionWith (+))
+                           . zipWith (curry (M.fromList
+                           . (: []) . (,1))) poly'
+                   $ tail poly'
+            polyCount = foldl1 (M.unionWith (+)) $
+                        map (M.fromList . (\n -> [(n,1)])) poly'
+
+newPoly ::  PolyChains -> Operation -> Poly
+newPoly polyCount (oper, to) = (new, M.fromList [(to, count)])
+      where count = M.findWithDefault 0 oper polyCount
+            new = M.fromList [((fst oper, to), count), ((to, snd oper), count)]
+
+findOper :: PolyChains -> [Operation] -> [Operation]
+findOper polyCount = filter (flip elem validOperKey . fst)
+      where validOperKey = M.keys polyCount
+
+applyOper :: [Operation] -> Poly -> Poly
+applyOper opers (poly, count) = (newPoly', newCount)
+      where opers' = findOper poly opers
+            polysWithCount = map (newPoly poly) opers'
+            newPoly' = foldl1 (M.unionWith (+)) $ map fst polysWithCount
+            newCount = foldl (M.unionWith (+)) count $ map snd polysWithCount
+
+applyNOper :: Int -> [Operation] -> Poly -> Poly
+applyNOper n oper = last . take (n + 1) . iterate (applyOper oper)
+
+solve :: Int -> [Operation] -> Poly -> Integer
+solve n oper poly = max' - min'
+      where poly' = applyNOper n oper poly
+            min' = minimum $ snd poly'
+            max' = maximum $ snd poly'
+
+main :: IO ()
+main = do
+      input <- getContents
+      let (oper, poly) = parse input
+      print $ solve 10 oper poly
+      print $ solve 40 oper poly
